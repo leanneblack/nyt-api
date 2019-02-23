a5c900017a9cabdd07fe72584e7eef96b7deaf86
Hoang Le <hoang.le4@pcc.edu>
2019-02-23 12:46:59 -0800

    commit

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 let xhttp = new XMLHttpRequest()
 
-  xhttp.onreadystatechange = function(e) {
-    
+  xhttp.onreadystatechange = function(res) {
+    if(this.readyState=== 4 && this.status === 200) {
+      console.log('success');
+    }
   }
 
-  xhttp.open();
+  xhttp.open('GET', 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=KDWT1LrF4Oq8WmVMLOkUynon9MZj82yL', true);
   xhttp.send();
